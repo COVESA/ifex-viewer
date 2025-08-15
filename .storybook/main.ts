02339514cdf1bbd1f9007e9253ddb8261af0d4bd
Pascal Bothner <34553760+pboth1304@users.noreply.github.com>
2025-08-15 09:20:06 +0200

    chore(deps): remove chromatic dependency from project (#16)
    Signed-off-by: Pascal Bothner <pascal.bothner@mercedes-benz.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions', '@storybook/addon-themes'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-themes'],
   framework: {
     name: '@storybook/vue3-vite',
     options: { docgen: 'vue-component-meta' },
